692871454c0ce5164181862ae0559e2e2ad30750
Madalin Cotetiu <cotetiumadalin@gmail.com>
2019-02-08 10:25:06 +0200

    added firefox tests

--- src/main/java/testMaven.java
+++ src/main/java/testMaven.java
@@ -2,8 +2,10 @@
  * Created by madalin.cotetiu on 2/7/2019.
  */
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.io.File;
 import java.nio.file.Path;
@@ -13,27 +15,45 @@ public class testMaven {
 
     public static void main(String[] args) {
 
-        System.setProperty("webdriver.chrome.driver", "chromedriver/chromedriver.exe");
+        //Chrome tests
+//        System.setProperty("webdriver.chrome.driver", "chromedriver/chromedriver.exe");
+//
+//        ChromeDriver chromeDriver = new ChromeDriver();
+//        chromeDriver.get("http://automationpractice.com/index.php");
+//
+//        WebElement loginLink = chromeDriver.findElementByClassName("login");
+//        loginLink.click();
+//
+//        WebElement emailField = chromeDriver.findElementById("email");
+//        WebElement passwordField = chromeDriver.findElementById("passwd");
+//        WebElement submitButton = chromeDriver.findElementById("SubmitLogin");
+//
+//        emailField.sendKeys("madalincotetiu@gmail.com");
+//        passwordField.sendKeys("test1923");
+//        submitButton.click();
+//
+//        chromeDriver.close();
 
-        ChromeDriver chromeDriver = new ChromeDriver();
-        chromeDriver.get("http://automationpractice.com/index.php");
 
-        WebElement loginLink = chromeDriver.findElementByClassName("login");
+
+        //Firefox tests
+        System.setProperty("webdriver.gecko.driver", "geckodriver/geckodriver.exe");
+
+        FirefoxDriver firefoxDriver = new FirefoxDriver();
+        firefoxDriver.get("http://automationpractice.com/index.php");
+
+        WebElement loginLink = firefoxDriver.findElementByClassName("login");
         loginLink.click();
 
-        WebElement emailField = chromeDriver.findElementById("email");
-        WebElement passwordField = chromeDriver.findElementById("passwd");
-        WebElement submitButton = chromeDriver.findElementById("SubmitLogin");
+        WebElement emailField = firefoxDriver.findElementById("email");
+        WebElement passwordField = firefoxDriver.findElementById("passwd");
+        WebElement submitButton = firefoxDriver.findElementById("SubmitLogin");
 
         emailField.sendKeys("madalincotetiu@gmail.com");
         passwordField.sendKeys("test1923");
         submitButton.click();
 
-        chromeDriver.close();
-
-
-
-
+        firefoxDriver.close();
 
     }
 }
